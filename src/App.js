6a8c6d6f0e77fdd24e86b094afb69cfd6cd11d5a
root <root@ndmaddhouse.com>
2021-08-26 20:17:15 -0500

    Implemented background effect in React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import WaterWave from 'react-water-wave';
+import logo from './logo-nobackground-alt.png';
 import './App.css';
 
-function App() {
+
+function App(props) {
+  const subject = props.subject;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <WaterWave 
+        style={{
+          width: "100%",
+          height: "100vh",
+          backgroundImage: `url(${logo})`,
+          backgroundPosition: "center center",
+          backgroundSize: "100% 150%"
+        }}
+        dropRadius={200}
+        perturbance={0.1}
+        interactive={true}
+      >
+      </WaterWave>
       </header>
+      <p className="App-hello">
+          Hello, {subject}!
+        </p>
     </div>
   );
 }
